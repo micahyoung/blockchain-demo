467db36129a4224b650a9dfd134964eacc6e2eb3
Micah Young <myoung@pivotal.io>
2017-10-21 10:26:09 -0700

    Parameterize up script

--- up.sh
+++ up.sh
@@ -3,11 +3,14 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+HOSTNAME=${1:?"!"}
+DOMAIN=${2:?"!"}
+MINER_URL=${3:?"!"}
 pushd sender_app
   mvn clean package -DskipTests
-  cf push sender -p target/*.jar --no-start
+  cf push sender -p target/*.jar --no-route --no-start
+  cf map-route -n $HOSTNAME $DOMAIN 
   
-  MINER_URL=https://eth-node-1.cfapps.io
   cf set-env sender SPRING_APPLICATION_JSON '{"config":{"nodeUrl":"'$MINER_URL'","senderAccountPassword":"password"}}'
   
   cf start sender
